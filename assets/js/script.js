b376f0e4b85ddd1153915570c72672e0f9d71900
HumaIlyas <hi.wtmconsult@gmail.com>
2022-10-18 12:28:10 +0000

    Add code (variables and functions) to check the result and display the message

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+// show result of the game 
+let result = document.getElementById("result");
+let comp_score = 0;
+let player_score = 0;
+
+let gameConditions = {
+    Rock: {
+        Rock: 'tie',
+        Paper: 'win',
+        Scissors: 'lose',
+        Lizard: 'lose',
+        Spock: 'win' 
+    },
+    Paper: {
+        Paper: 'tie',
+        Rock: 'lose',
+        Scissors: 'win',
+        Lizard: 'win',
+        Spock: 'lose'
+    },
+    Scissors: {
+        Scissors: 'tie',
+        Paper: 'lose',
+        Rock: 'win',
+        Lizard: 'lose',
+        Spock: 'win'
+    },
+    Lizard: {
+        Lizard: 'tie',
+        Scissors: 'win',
+        Paper: 'lose',
+        Rock: 'win',
+        Spock: 'lose'
+    },
+    Spock: {
+        Spock: 'tie',
+        Lizard: 'win',
+        Scissors: 'lose',
+        Paper: 'win',
+        Rock: 'lose',
+    }
+}
+
+// For the player to click the choice
+// To generate a random choice of the computer 
+// To display choices and show a message about the result
+
+function clicked(input) {
+    let choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+    let randomNumber = Math.trunc(Math.random() * 5);
+    let comp_choice = choices[randomNumber];
+
+    console.log(randomNumber);
+    document.getElementById("playerChoice").innerHTML = `Computer chose: <span>${comp_choice.toUpperCase()}</span>`;
+    document.getElementById("compChoice").innerHTML = `You chose: <span>${input.toUpperCase()}</span>`;
+
+    console.log('input', input, 'comp-choice', comp_choice);
+
+    // Compare the computer choice with player choice and update the scores of both of them
+    switch(gameConditions[comp_choice][input]) {
+        case 'win':
+            result.innerText = `You Won!`;
+            result.style.cssText = "background-color: rgb(128, 247, 128)";
+            player_score++;
+            document.getElementById("player_score").innerHTML = player_score
+            console.log("PS: ", player_score);
+            break;
+        case 'lose':
+            result.innerText = `Computer Won!`;
+            result.style.cssText = "background-color: rgb(240, 124, 124)";
+            comp_score++;
+            document.getElementById("comp_score").innerHTML = comp_score
+            console.log("CS: ", comp_score);
+            break;
+        case 'tie':
+            result.innerText = "It's a tie!";
+            result.style.cssText = "background-color: rgb(168, 161, 161)";
+            break;  
+    }
+}
